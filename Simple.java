60f653734c48b513cd666ac1e984548e62331385
bhasker <support@edureka.co>
2022-09-07 17:04:48 +0000

    Modified Simple.java and index.html

--- Simple.java
+++ Simple.java
@@ -1,6 +1,6 @@
 class Simple{
    public static void main(String args[]) {
-   System.out.println("Hello Edureka, Welcome to Git's Online Training");
+   System.out.println("Welcome to Git's Online Training");
    }
 
 }
